ed7cb01a06861d01ed906e58581f2d5b7fee7ac2
endday <845426454@qq.com>
2020-04-14 14:20:10 +0800

    add DeepL translate

--- src/config/engines/translate.js
+++ src/config/engines/translate.js
@@ -9,29 +9,14 @@ export default [
   },
   {
     name: '有道词典',
-    url: 'http://dict.youdao.com/search?q=%s',
+    url: 'http://dict.youdao.com/search?q=%s'
   },
   {
     name: '必应翻译',
     url: 'http://cn.bing.com/dict/search?q=%s'
   },
   {
-    name: 'Forvo翻译',
-    url: 'https://zh.forvo.com/search/%s',
-    blank: true
-  },
-  {
-    name: 'CNKI翻译',
-    url: 'http://dict.cnki.net/dict_result.aspx?searchword=%s',
-    disable: true
-  },
-  {
-    name: '汉典',
-    url: 'http://www.zdic.net/sousuo/?q=%s',
-    disable: true
-  },
-  {
-    name: '海词',
-    url: 'http://dict.cn/%s'
+    name: 'DeepL',
+    url: 'https://www.deepl.com/translator#zh/en/%s'
   }
 ]
