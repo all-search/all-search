be0a0fd5610e43a3fab355590462f5581d263a12
endday <845426454@qq.com>
2021-04-18 23:42:55 +0800

    commit

---
+++ src/as-script/components/overlay.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="as-overlay"
+       @mousedown="onMouseDown"
+       @mouseup="onMouseUp"
+       @click="onMaskClick">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'overlay',
+  setup (props, { emit }) {
+    let mouseDownTarget = false
+    let mouseUpTarget = false
+    const onMaskClick = (e) => {
+      if (mouseDownTarget && mouseUpTarget) {
+        emit('click', e)
+      }
+      mouseDownTarget = mouseUpTarget = false
+    }
+    const onMouseDown = (e) => {
+      mouseDownTarget = e.target === e.currentTarget
+    }
+    const onMouseUp = (e) => {
+      mouseUpTarget = e.target === e.currentTarget
+    }
+
+    return {
+      onMouseDown,
+      onMouseUp,
+      onMaskClick
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../../assets/common.scss";
+
+  .as-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: $overlayZIndex;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    overflow: auto;
+  }
+</style>
--- src/as-script/components/side-bar.vue
+++ src/as-script/components/side-bar.vue
@@ -5,53 +5,47 @@
     设置
   </div>
   <teleport to="html">
-    <div class="as-overlay"
-         v-show="visible"
-         @mousedown="onMouseDown"
-         @mouseup="onMouseUp"
-         @click="onMaskClick">
-      <div class="as-side-bar">
-        <header></header>
-        <section></section>
-      </div>
-    </div>
+    <transition name="overlay-fade" appear>
+      <overlay
+        v-show="visible"
+        @click="onMaskClick">
+        <transition name="drawer" appear>
+          <div
+            v-show="visible"
+            aria-modal="true"
+            role="dialog"
+            class="as-side-bar"
+            :class="{open: visible}"
+            @click.stop>
+            <header></header>
+            <section></section>
+          </div>
+        </transition>
+      </overlay>
+    </transition>
   </teleport>
 </template>
 
 <script>
 import { ref } from 'vue'
+import overlay from '../components/overlay'
 
 export default {
   name: 'side-bar',
+  components: {
+    overlay
+  },
   setup () {
-
-
     const visible = ref(false)
     const open = () => {
       visible.value = true
     }
-    // const close = () => {
-    //   visible.value = false
-    // }
-    let mousedownTarget = false
-    let mouseupTarget = false
-    const onMaskClick = (e) => {
-      if (mousedownTarget && mouseupTarget) {
-        visible.value = false
-      }
-      mousedownTarget = mouseupTarget = false
-    }
-    const onMouseDown = (e) => {
-      mousedownTarget = e.target === e.currentTarget
-    }
-    const onMouseUp = (e) => {
-      mouseupTarget = e.target === e.currentTarget
+    const onMaskClick = () => {
+      visible.value = false
     }
     return {
       visible,
       open,
-      onMouseDown,
-      onMouseUp,
       onMaskClick
     }
   }
@@ -61,18 +55,6 @@ export default {
 
 <style lang="scss">
   @import "../../assets/common.scss";
-
-  .as-overlay {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: $overlayZIndex;
-    height: 100%;
-    background-color: rgba(0, 0, 0, .5);
-    overflow: auto;
-  }
 
   .as-side-bar {
     width: 30%;
@@ -87,5 +69,28 @@ export default {
     flex-direction: column;
     box-shadow: 0 8px 10px -5px rgba(0, 0, 0, .2), 0 16px 24px 2px rgba(0, 0, 0, .14), 0 6px 30px 5px rgba(0, 0, 0, .12);
     overflow: hidden;
+  }
+
+  .as-side-bar.open {
+    /*animation: rtl-drawer-in .9s;*/
+  }
+
+  .overlay-fade-enter-active, .overlay-fade-leave-active {
+    transition: opacity .5s;
+    > .as-side-bar {
+      opacity: 1;
+    }
+  }
+
+  .overlay-fade-enter-from, .overlay-fade-leave-to {
+    opacity: 0;
+  }
+
+  .drawer-enter-active, .drawer-leave-active {
+    transition: transform .5s;
+  }
+
+  .drawer-enter-from, .drawer-leave-to {
+    transform: translate(100%);
   }
 </style>
