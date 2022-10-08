bc3ef8133812015b3c143478ad62aba0998af850
endday <845426454@qq.com>
2022-10-08 19:24:39 +0800

    feat: mutation observe

--- src/util/initStyle.js
+++ src/util/initStyle.js
@@ -57,6 +57,91 @@ export const initStyle = function () {
 
 export const addStyleForCurrentSite = function (mode, site, remove = false) {
   const modeVal = unref(mode)
-  addCustomStyle(modeVal, site, remove)
+  // addCustomStyle(modeVal, site, remove)
+  addSpecialStyle()
   initBodyClass(modeVal, site, remove)
+}
+
+function modifyPosition (item) {
+  const style = window.getComputedStyle(item)
+  const display = style.getPropertyValue('display')
+  if (display === 'none') {
+    return
+  }
+  const position = style.getPropertyValue('position')
+  if (position === 'fixed') {
+    changeTop(item)
+  } else if (position === 'absolute') {
+    let parent = item.offsetParent
+    let target = item
+    while (parent && parent.tagName !== 'BODY') {
+      target = parent
+      const offsetParent = parent.offsetParent
+      if (offsetParent && offsetParent.tagName !== 'BODY') {
+        parent = offsetParent
+      } else {
+        parent = null
+      }
+    }
+    changeTop(target)
+  }
+}
+
+function getTop (el) {
+  const style = window.getComputedStyle(el)
+  return style.getPropertyValue('top') || '0px'
+}
+
+function changeTop (item) {
+  const top = getTop(item)
+  if (top.includes('px')) {
+    const num = parseInt(top.replace('px', '')) || 0
+    item.style.top = `${num + 30}px`
+  }
+}
+
+const fixedNodeMap = new WeakMap()
+
+let isSelfChange = false
+
+function addSpecialStyle () {
+  console.log('addSpecialStyle')
+  document.body.querySelectorAll('*').forEach(item => {
+    modifyPosition(item)
+  })
+  // 选择需要观察变动的节点
+  const targetNode = document.body
+  // 观察器的配置（需要观察什么变动）
+  const config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    attributeFilter: ['style', 'class']
+  }
+  // 当观察到变动时执行的回调函数
+  const callback = function (mutationsList) {
+    if (isSelfChange) {
+      isSelfChange = false
+    } else {
+      isSelfChange = true
+      setTimeout(()=> {
+        for (let mutation of mutationsList) {
+          if (mutation.type === 'attributes') {
+            const item = mutation.target
+            if (!['BODY', 'STYLE'].includes(item.tagName)) {
+              modifyPosition(item)
+            }
+          }
+        }
+      })
+    }
+    console.log(isSelfChange)
+  }
+  // 创建一个观察器实例并传入回调函数
+  const observer = new MutationObserver(callback)
+  // 以上述配置开始观察目标节点
+  observer.observe(targetNode, config)
+  // 之后，可停止观察
+  //   observer.disconnect();
 }
