9a2cff9192de115a034de9b46c51e97a1e0c222f
endday <845426454@qq.com>
2022-10-28 15:30:36 +0800

    fix: [search.nixos.org] will break If I use iconfont

--- src/as-script/main.js
+++ src/as-script/main.js
@@ -6,26 +6,21 @@ import {
   passTmMethods
 } from '../util/index'
 import { protectStyle } from '../util/initStyle.js'
-import '../assets/iconfont'
+import iconfont from '../assets/iconfont'
 import { siteInfo } from '../config/loadList.js'
 
 const site = siteInfo()
 
-const app = createApp(index)
-
-function init () {
+if (!site.disabled) {
+  iconfont()
+  passTmMethods()
+  protectStyle()
   const el = getAsRoot()
   if (!el) {
+    const app = createApp(index)
     const el = createAsRoot()
     const mountEL = document.documentElement.insertBefore(el, document.body)
     app.mount(mountEL)
   }
   console.log(`all-search running 全搜运行中(${process.env.NODE_ENV})`)
-}
-
-passTmMethods()
-
-if (!site.disabled) {
-  protectStyle()
-  init()
 }
